eafd590da60b9b4eff66e27af07f925a9cc5e6d8
nahid <shams.nahid@ixorasolution.com>
2019-10-27 13:10:26 +0600

    added rpc

---
+++ test1.js
@@ -0,0 +1,22 @@
+// rpcuser=multichainrpc
+// rpcpassword=BtKMa3Aqs1tqLQNUJYEfWQ2FjeLQquvL7xBL5QzHQKHL
+
+let multichain = require("multichain-node")({
+  port: 8332,
+  host: '54.144.208.138',
+  user: "multichainrpc",
+  pass: "BtKMa3Aqs1tqLQNUJYEfWQ2FjeLQquvL7xBL5QzHQKHL"
+});
+
+multichain.getInfo((err, info) => {
+  if(err){
+      throw err;
+  }
+  console.log(info);
+})
+
+
+// /root/.multichain/my-blockchain/multichain.conf should be
+// rpcuser=user
+// rpcpassword=password
+// rpcallowip=0.0.0.0/0 // added this line
